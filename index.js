044f9b5469eea0a9ebfe035cb99b3153385f041c
thedarknightassasin <53987634+thedarknightassasin@users.noreply.github.com>
2021-06-27 22:20:53 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+
+document.querySelector('.confirmButton').addEventListener('click', function(){
+  const faccName =  document.querySelector('.myName').value;
+  document.querySelector('h1').textContent = `FUKC YOU ${faccName}`
+})
